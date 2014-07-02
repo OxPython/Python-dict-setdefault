c26eb988d2af04fef52dcc29a61f37c9c2da1e23
Emerson <viejoemer@gmail.com>
2014-07-02 10:45:28 -0500

    How I can get a value for default if that value does not exist in the dict in Python?

---
+++ dict_setdefault.py
@@ -0,0 +1,25 @@
+'''
+Created on Jul 2, 2014
+
+@author: viejoemer
+'''
+############################################################
+#How I can get a value for default if that value does not exist in the dict in Python?
+
+#The method setdefault() is similar to get(), 
+#but will set dict[key]=default if key is not already in dict.
+
+#Creating a dict with data
+
+
+d = {'Name': 'Emerson', 'Sex': "M"}
+
+print (d["Name"], d["Sex"], d.setdefault('Age', None))
+
+print(d)
+
+d.setdefault('Name', None)
+
+print(d)
+
+print(d)
